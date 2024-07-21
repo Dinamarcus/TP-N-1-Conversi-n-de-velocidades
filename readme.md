d577bdd223e5fb9113e3701fba0f604e46bd73a6
Dinamarcus <fadimarco@frba.utn.edu.ar>
2024-07-21 19:30:52 -0300

    primer commit

--- readme.md
+++
@@ -1,3 +0,0 @@
-#### Trabajo Práctico N°1 - Conversión de velocidades
-### Proyecto con varios fuentes y makefile
-
--- tabla.c
+++ tabla.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <assert.h> 
 #include "conversion.h"
 #include "tabla.h"
 
 void tabla (int nrofilas, float nudos[]) {
+    assert(nrofilas > 0);
+    assert(nudos != NULL);
+
     printf("NUDOS  |\tKMH|\n");
     for(int i = 0; i < nrofilas; i++){
         printf("%g  |\t%g|\n", nudos[i], conversion(nudos[i]));
